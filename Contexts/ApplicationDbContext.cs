7d5b41de597f76b2ee7d6828f11f9ff30da2fe5a
Nahuel Bentos <nahuelbentosgnocchi@gmail.com>
2020-09-10 01:02:51 -0300

    Initial commit

---
+++ Contexts/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiPrimerWebApi_M3.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiPrimerWebApi_M3.Contexts
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Autor> Autores{ get; set; }
+    }
+}
---
+++ Controllers/AutorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiPrimerWebApi_M3.Controllers
+{   
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutorController: ControllerBase
+    {
+
+
+    }
+}
